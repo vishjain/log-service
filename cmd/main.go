74f29cc46af64effeab588c32749b43f310482b9
Vishal Jain <vishaljain97@gmail.com>
2021-08-24 18:47:14 -0700

    Fixed server handler & integrated with newly written File Manager.

--- cmd/main.go
+++ cmd/main.go
@@ -3,43 +3,39 @@ package main
 import (
 	"errors"
 	"fmt"
-	"log"
+	processing "github.com/vishjain/log-service/processing"
 	"net/http"
 	"strconv"
 	"sync"
-	"time"
 )
 
 func main() {
 	nc := NewNotificationCenter()
 
-	go func() {
-		for {
-			b := []byte(time.Now().Format(time.RFC3339))
-			if err := nc.Notify(b); err != nil {
-				log.Fatal(err)
-			}
-
-			time.Sleep(1 * time.Second)
-		}
-	}()
-
-	http.HandleFunc("/query", handleLogQuery(nc))
+	//go func() {
+	//	for {
+	//		b := []byte(time.Now().Format(time.RFC3339))
+	//		if err := nc.Notify(b); err != nil {
+	//			log.Fatal(err)
+	//		}
+	//
+	//		time.Sleep(1 * time.Second)
+	//	}
+	//}()
+	fileManager := processing.NewFileManager(5)
+	http.HandleFunc("/query", handleLogQuery(nc, fileManager))
 	http.ListenAndServe(":8001", nil)
 }
 
 type UnsubscribeFunc func() error
 
 type Subscriber interface {
-	Subscribe(c chan []byte) (UnsubscribeFunc, error)
-}
-type QueryParams struct {
-	fileName string
-	lastNEvents int
-	includeFilterStr string
+	Subscribe(c chan *processing.FileBlockReadInfo) (UnsubscribeFunc, error)
 }
 
-func parseAndValidateQueryValues(r *http.Request) (*QueryParams, error) {
+
+
+func parseAndValidateQueryValues(r *http.Request) (*processing.QueryParams, error) {
 	values := r.URL.Query()
 	var fileName string
 	var lastNEvents int
@@ -70,8 +66,8 @@ func parseAndValidateQueryValues(r *http.Request) (*QueryParams, error) {
 			return nil, err
 		}
 
-		// Number must be greater than 0.
-		if lastNEvents <= 0 {
+		// Number must be greater or equal to 0.
+		if lastNEvents < 0 {
 			return nil, errors.New("Provide a positive number of events to see.")
 		}
 	}
@@ -86,14 +82,14 @@ func parseAndValidateQueryValues(r *http.Request) (*QueryParams, error) {
 		includeFilterStr = includeFilterStrLst[0]
 	}
 
-	return &QueryParams{
-		fileName: fileName,
-		lastNEvents: lastNEvents,
-		includeFilterStr: includeFilterStr,
+	return &processing.QueryParams{
+		FileName: fileName,
+		LastNEvents: lastNEvents,
+		IncludeFilterStr: includeFilterStr,
 	}, nil
 }
 
-func handleLogQuery(s Subscriber) http.HandlerFunc {
+func handleLogQuery(s Subscriber, fileManager *processing.FileManager) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 
 		// Quick check on the input of the http request.
@@ -104,13 +100,17 @@ func handleLogQuery(s Subscriber) http.HandlerFunc {
 		}
 		fmt.Printf("Query params %+v \n", *queryParams)
 
-		// Subscribe
-		c := make(chan []byte)
+		// Subscribe channel.
+		c := make(chan *processing.FileBlockReadInfo)
 		unsubscribeFn, err := s.Subscribe(c)
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
+
+		go func() {
+			fileManager.ProcessLogQuery(c, queryParams)
+		}()
 
 		// Signal SSE Support
 		w.Header().Set("Content-Type", "text/event-stream")
@@ -118,7 +118,7 @@ func handleLogQuery(s Subscriber) http.HandlerFunc {
 		w.Header().Set("Connection", "keep-alive")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		i := 10
+
 		for {
 			select {
 			case <-r.Context().Done():
@@ -128,35 +128,44 @@ func handleLogQuery(s Subscriber) http.HandlerFunc {
 				}
 
 			default:
-				b := <-c
-				fmt.Fprintf(w, "data: %s\n\n", b)
-				i -= 1
-				if i <= 0 {
+				fileBlockReadInfo := <-c
+
+				// Error out if there has been an error.
+				if fileBlockReadInfo.Err != nil {
+					http.Error(w, fileBlockReadInfo.Err.Error(), http.StatusInternalServerError)
 					return
 				}
+
+				// Write all lines read & flush.
+				for _, line := range fileBlockReadInfo.FileBlockRead {
+					_, err := fmt.Fprintf(w, "%s \n", line)
+					if err != nil {
+						http.Error(w, err.Error(), http.StatusInternalServerError)
+						return
+					}
+				}
 				w.(http.Flusher).Flush()
+				if fileBlockReadInfo.FileProcessingFinished {
+					return
+				}
 			}
 		}
 	}
 }
 
-type Notifier interface {
-	Notify(b []byte) error
-}
-
 type NotificationCenter struct {
-	subscribers   map[chan []byte]struct{}
+	subscribers   map[chan *processing.FileBlockReadInfo]struct{}
 	subscribersMu *sync.Mutex
 }
 
 func NewNotificationCenter() *NotificationCenter {
 	return &NotificationCenter{
-		subscribers:   map[chan []byte]struct{}{},
+		subscribers:   map[chan *processing.FileBlockReadInfo]struct{}{},
 		subscribersMu: &sync.Mutex{},
 	}
 }
 
-func (nc *NotificationCenter) Subscribe(c chan []byte) (UnsubscribeFunc, error) {
+func (nc *NotificationCenter) Subscribe(c chan *processing.FileBlockReadInfo) (UnsubscribeFunc, error) {
 	nc.subscribersMu.Lock()
 	nc.subscribers[c] = struct{}{}
 	nc.subscribersMu.Unlock()
@@ -172,16 +181,16 @@ func (nc *NotificationCenter) Subscribe(c chan []byte) (UnsubscribeFunc, error) 
 	return unsubscribeFn, nil
 }
 
-func (nc *NotificationCenter) Notify(b []byte) error {
-	nc.subscribersMu.Lock()
-	defer nc.subscribersMu.Unlock()
-
-	for c := range nc.subscribers {
-		select {
-		case c <- b:
-		default:
-		}
-	}
-
-	return nil
-}
+//func (nc *NotificationCenter) Notify(b []byte) error {
+//	nc.subscribersMu.Lock()
+//	defer nc.subscribersMu.Unlock()
+//
+//	for c := range nc.subscribers {
+//		select {
+//		case c <- b:
+//		default:
+//		}
+//	}
+//
+//	return nil
+//}
---
+++ processing/file_manager.go
@@ -0,0 +1,120 @@
+package processing
+
+import (
+	"fmt"
+)
+
+type FileManager struct {
+	fileToFileProcessor map[string]*FileProcessor
+	maxLinesToRetrieve int
+}
+
+
+func NewFileManager(maxLinesToRetrieve int) *FileManager {
+	return &FileManager{
+		fileToFileProcessor: make(map[string]*FileProcessor),
+		maxLinesToRetrieve: maxLinesToRetrieve,
+	}
+}
+
+type QueryParams struct {
+	FileName string
+	LastNEvents int
+	IncludeFilterStr string
+}
+
+type FileBlockReadInfo struct {
+	FileBlockRead []string
+	FileProcessingFinished bool
+	Err error
+}
+
+func (fm *FileManager) ProcessLogQuery(c chan *FileBlockReadInfo, queryParams *QueryParams) {
+	// Check if the file exists in map.
+	var fileProcessor *FileProcessor
+	var ok bool
+
+	fileProcessor, ok = fm.fileToFileProcessor[queryParams.FileName]
+	if !ok {
+		fileProcessor = NewFileProcessor(queryParams.FileName)
+	}
+
+	// Open File & Call File Stat.
+	file, fileInfo, err := fileProcessor.OpenFileAndStat()
+	if err != nil {
+		fmt.Printf("File Processor Error: %v \n", err)
+		c <- &FileBlockReadInfo{
+			Err: err,
+		}
+	}
+	defer fileProcessor.FileClose()
+
+	logScanner := NewLogScanner(file, int(fileInfo.Size()) - 1, 2048)
+	if queryParams.LastNEvents > 0 {
+		fm.processLastNEvents(fileProcessor, logScanner, c, queryParams)
+	} else {
+		fm.processWholeFile(fileProcessor, logScanner, c, queryParams)
+	}
+
+}
+
+
+func (fm *FileManager) processWholeFile(fileProcessor *FileProcessor, logScanner *LogScanner, c chan *FileBlockReadInfo, queryParams *QueryParams) {
+	return
+}
+
+func (fm *FileManager) processLastNEvents(fileProcessor *FileProcessor, logScanner *LogScanner,
+	c chan *FileBlockReadInfo, queryParams *QueryParams) {
+
+	filter := &FileFilter {
+		noFilter: queryParams.IncludeFilterStr == "",
+		includeString: queryParams.IncludeFilterStr,
+	}
+
+	totalNEventsRemaining := queryParams.LastNEvents
+	for {
+		if totalNEventsRemaining <= 0 {
+			c <- &FileBlockReadInfo{
+				FileBlockRead:          nil,
+				FileProcessingFinished: true,
+				Err:                    nil,
+			}
+			return
+		}
+
+		linesToRetrieve := fm.maxLinesToRetrieve
+		if totalNEventsRemaining < linesToRetrieve {
+			linesToRetrieve = totalNEventsRemaining
+		}
+
+		resp, err := fileProcessor.RetrieveNextFileEvents(logScanner, filter, &RetrieveParams{
+			maxLinesToRetrieve:linesToRetrieve,
+		})
+		if err != nil {
+			fmt.Printf("processLastNEvents Error: %v \n", err)
+			c <- &FileBlockReadInfo{
+				Err: err,
+			}
+			return
+		}
+
+		// Send block of lines read to server.
+		c <- &FileBlockReadInfo{
+			FileBlockRead:          resp.lineList,
+			FileProcessingFinished: false,
+			Err:                    nil,
+		}
+		totalNEventsRemaining -= len(resp.lineList)
+
+		// If the file has reached end of file, send to server.
+		if resp.eof {
+			fmt.Printf("End of file, processing complete. \n")
+			c <- &FileBlockReadInfo{
+				FileBlockRead:          nil,
+				FileProcessingFinished: true,
+				Err:                    nil,
+			}
+		}
+	}
+
+}
